509b6df9686c6de67881a90e390c8af133a016e6
Your Name <15121816502@163.com>
2019-12-18 23:17:49 +0800

    前端验证表单格式完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,17 +4,19 @@
         <div style="text-align: center">
             <img src="../../assets/img/logo_index.png" alt="" id="picLogo">
         </div>
-        <el-form>
-            <el-form-item>
-                <el-input placeholder="请输入手机号" style="margin-top: 10px"></el-input>
+        <el-form :model="formData" :rules="loginRule" ref="myForm">
+            <el-form-item prop="phone">
+                <el-input v-model="formData.phone" placeholder="请输入手机号" style="margin-top: 10px"></el-input>
             </el-form-item>
-             <el-form-item>
-                <el-input placeholder="请输入验证码" style="width: 65%" id="Vcode" ></el-input>
+             <el-form-item prop="code">
+                <el-input v-model='formData.code' placeholder="请输入验证码" style="width: 65%" id="Vcode" ></el-input>
                 <el-button type='info' style="float: right">获取验证码</el-button>
             </el-form-item>
-            <el-checkbox  style="margin-top: 10px">是否同意我侵犯你的权益</el-checkbox>
-            <el-form-item>
-                <el-button type='info' style="width: 100%; margin-top: 20px">登陆</el-button>
+            <el-form-item prop="check">
+                <el-checkbox v-model="formData.check">是否同意我侵犯你的权益</el-checkbox>
+            </el-form-item>
+             <el-form-item>
+                <el-button type='info' style="width: 100%" @click="submitKey">登陆</el-button>
             </el-form-item>
         </el-form>
     </el-card>
@@ -22,19 +24,52 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      formData: {
+        phone: '',
+        code: '',
+        check: false
+      },
+      loginRule: {
+        phone: [{ required: true, message: '请输入手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '输入格式不正确' }],
+        code: [{ required: true, message: '请输入验证码' }, {
+          pattern: /^\d{6}$/, message: '验证码错误'
+        }],
+        check: [{ validator: function (rule, value, callback) {
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('请确认你的选择'))
+          }
+        } }]
+      }
+    }
+  },
+  methods: {
+    submitKey () {
+      this.$refs.myForm.validate(function (isOk) {
+        if (isOk) {
+          console.log(isOk)
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang='less' scope>
 .bgc {
   height: 100vh;
-  background: url("../../assets/img/F201111281505181366600000.jpg");
+  background:  url("../../assets/img/F201111281505181366600000.jpg");
   background-size: contain;
   opacity: 0.7;
 }
 .carbox {
-    width: 450px;
-    height: 300px;
+    width: 470px;
+    height: 320px;
     position: relative;
     left: 50%;
     top:50%;
