1fdbcb0b281b938950a2868e34b60319d06e35f3
Your Name <15121816502@163.com>
2019-12-18 16:14:57 +0800

    文件初始化及背景完成

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <div class="bgc"></div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .bgc {
+    height: 100vh;
+    background: url('./assets/img/F201111281505181366600000.jpg');
+    background-size: contain;
+    opacity: 0.7;
   }
-}
 </style>
