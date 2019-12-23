d8d0a4f83606dbd8003f4861bd94e38a2ca05350
Your Name <15121816502@163.com>
2019-12-23 12:11:40 +0800

    图片素材上传成功

--- src/views/home/meteri.vue
+++ src/views/home/meteri.vue
@@ -1,37 +1,58 @@
 <template>
   <el-card>
       <common slot="header">
-          <template slot="title">素材管理</template>
+        <template slot="title">素材管理</template>
       </common>
-      <el-tabs  type="card" v-model="activeName" @tab-click="changeTab">
-          <el-tab-pane label="全部素材"  name="all">
-              <!-- 放全部素材 -->
-              <div class="uaerAllPic">
-                  <el-card v-for="item in list" :key = 'item.id' class="userPicture" >
-                      <div class="imgPicBox">
-                      <img :src="item.url" alt="" class="imgPic" >
+      <el-row type="flex" justify="end">
+        <el-upload :show-file-list = 'false' :http-request = 'uploadImg'>
+          <el-button size="small" type="primary">点击上传</el-button>
+        </el-upload>
+      </el-row>
 
-                      </div>
-                      <div class="icon">
-                          <i class="el-icon-star-on"></i>
-                          <i class="el-icon-delete-solid"></i>
-                      </div>
-                  </el-card>
-              </div>
-          </el-tab-pane>
-          <el-tab-pane label="收藏素材"  name="collect">
-              <!-- 放收藏素材 -->
-              <div class="uaerAllPic">
-                  <el-card v-for="item in list" :key = 'item.id' class="userPicture" >
-                      <div class="imgPicBox">
-                      <img :src="item.url" alt="" class="imgPic" >
-
-                      </div>
-
-                  </el-card>
-              </div>
-          </el-tab-pane>
-      </el-tabs>
+    <el-tabs type="card" v-model="activeName" @tab-click="changeTab">
+      <el-tab-pane label="全部素材" name="all">
+        <!-- 放全部素材 -->
+        <div class="uaerAllPic">
+          <el-card v-for="item in list" :key="item.id" class="userPicture">
+            <div class="imgPicBox">
+              <img :src="item.url" alt class="imgPic" />
+            </div>
+            <div class="icon">
+              <i class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
+            </div>
+          </el-card>
+            <el-row type='flex' style='height: 80px; width: 100%' justify="center" align="middle" >
+              <el-pagination background
+              layout="prev, pager, next"
+                :total="page.total"
+                :page-size = 'page.pageSize'
+                :current-page = 'page.currentPage'
+                @current-change = "changePage"
+               ></el-pagination>
+            </el-row>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="收藏素材" name="collect">
+        <!-- 放收藏素材 -->
+        <div class="uaerAllPic">
+          <el-card v-for="item in list" :key="item.id" class="userPicture">
+            <div class="imgPicBox">
+              <img :src="item.url" alt class="imgPic" />
+            </div>
+          </el-card>
+           <el-row type='flex' style='height: 80px; width: 100%' justify="center" align="middle" >
+              <el-pagination background
+              layout="prev, pager, next"
+                :total="page.total"
+                :page-size = 'page.pageSize'
+                :current-page = 'page.currentPage'
+                @current-change = "changePage"
+               ></el-pagination>
+            </el-row>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
@@ -40,22 +61,48 @@ export default {
   data () {
     return {
       activeName: 'all',
-      list: []
+      list: [],
+      page: {
+        pageSize: 8,
+        total: 0,
+        currentPage: 1
+      }
     }
   },
   methods: {
+    uploadImg (params) {
+      let form = new FormData()
+      form.append('image', params.file)
+      this.$http({
+        url: '/user/images',
+        data: form,
+        method: 'post'
+      }).then((res) => {
+        this.getPic()
+        console.log(res)
+      })
+      console.log(params)
+    },
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getPic()
+    },
     changeTab () {
+      this.page.currentPage = 1
       this.getPic()
     },
     getPic () {
       this.$http({
         url: 'user/images',
         params: {
-          collect: this.activeName === 'collect'
+          collect: this.activeName === 'collect',
+          page: this.page.currentPage,
+          per_page: this.page.pageSize
         }
-      }).then((res) => {
+      }).then(res => {
         this.list = res.data.results
-        console.log(this.list)
+        this.page.total = res.data.total_count
+        console.log(res.data.total_count)
       })
     }
   },
@@ -66,39 +113,36 @@ export default {
 </script>
 
 <style>
-    .uaerAllPic {
-        display: flex;
-        flex-wrap: wrap
-    }
-     .userPicture{
-         width: 230px;
-        padding: 0;
-         margin: 25px 35px;
-
-     }
-     .imgPicBox {
-        width: 190px;
-        height: 190px;
-        border-radius: 8px;
-        overflow: hidden;
-     }
-    .imgPic{
-       width: 100%;
-       height: 100%;
-
-    }
-    .icon  {
-        width: 230px;
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        margin-top: 15px;
-        font-size: 18px;
-        background-color: #f4f5f6;
-        height: 45px;
-        line-height: 45px;
-        margin-bottom: -20px;
-        margin-left: -20px
-
-    }
+.uaerAllPic {
+  display: flex;
+  flex-wrap: wrap;
+}
+.userPicture {
+  width: 230px;
+  padding: 0;
+  margin: 25px 35px;
+}
+.imgPicBox {
+  width: 190px;
+  height: 190px;
+  border-radius: 8px;
+  overflow: hidden;
+}
+.imgPic {
+  width: 100%;
+  height: 100%;
+}
+.icon {
+  width: 230px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 15px;
+  font-size: 18px;
+  background-color: #f4f5f6;
+  height: 45px;
+  line-height: 45px;
+  margin-bottom: -20px;
+  margin-left: -20px;
+}
 </style>
