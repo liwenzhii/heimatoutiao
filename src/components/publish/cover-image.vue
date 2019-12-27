841be602647e47483932deb6f1bd62b6a0ee66eb
Your Name <15121816502@163.com>
2019-12-27 14:25:54 +0800

    dddddd

---
+++ src/components/publish/cover-image.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="coverPicContainer">
+  <div v-for="(item, index) in imageList"  :key="index"  class="coverPic">
+    <div style="text-align: center; margin-top: 15px">点击上传图片</div>
+      <img src="./../../assets/img/pic_bg.png" alt="" class="imgs">
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+
+  props: ['imageList']
+
+}
+</script>
+
+<style scoped  lang = 'less'>
+    .coverPicContainer {
+         /* display: flex; */
+        display: inline-block;
+        border: 1px solid rgb(173, 168, 168);
+        margin-left: 50px
+    }
+    .coverPic {
+        float: left;
+    }
+    .imgs {
+        width: 200px;
+        height: 200px;
+        vertical-align: middle;
+
+    }
+</style>
